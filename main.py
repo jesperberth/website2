d198724d16b76c0148ba3bcd64871ecd3846d42a
Jesper Berth <jesper.berth@arrow.com>
2019-04-11 10:52:58 +0200

    second comit

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/user/<username>")
-def hello(username):
+def user(username):
     return "Hello World! %s " % username
 
+@app.route("/product/")
+def product():
+    return "Product Site"
 
 if __name__ == '__main__':
     app.run(debug=True)
